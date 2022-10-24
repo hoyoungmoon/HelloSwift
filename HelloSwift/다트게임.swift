4f0619350f5a4aaa6d5c989f65d8274d3de64af2
hoyoungmoon <moonhy1115@gmail.com>
2022-10-24 23:56:55 +0900

    add: 다트게임 답안

---
+++ HelloSwift/다트게임.swift
@@ -0,0 +1,39 @@
+//
+//  다트게임.swift
+//  HelloSwift
+//
+//  Created by mac on 2022/10/24.
+//
+
+import Foundation
+
+class 다트게임 {
+    static ㅌfunc solution(_ dartResult: String) -> Int {
+        let numbers = dartResult.split(whereSeparator: { !$0.isNumber }).compactMap { Int($0) }
+        let letters = dartResult.split(whereSeparator: { $0.isNumber })
+        var scores = Array(repeating: 0, count: numbers.count)
+
+        for (index, letter) in letters.enumerated() {
+            letter.forEach {
+                let number = numbers[index]
+                switch $0 {
+                case "D":
+                    scores[index] = number * number
+                case "T":
+                    scores[index] = number * number * number
+                case "#":
+                    scores[index] = -number
+                case "*":
+                    if index != 0 {
+                        scores[index - 1] = 2 * scores[index - 1]
+                    }
+                    scores[index] = 2 * scores[index]
+                default:
+                    break
+                }
+            }
+        }
+
+        return scores.reduce(0) { $0 + $1 }
+    }
+}
